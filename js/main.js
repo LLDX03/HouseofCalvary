fafe94bf95794ef6ca1b1303553fdbcd27d2849f
Leo <95978312+yaefdb@users.noreply.github.com>
2026-06-26 22:36:43 +0800

    update

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ const WA_SVG = `<svg viewBox="0 0 24 24"><path d="M17.472 14.382c-.297-.149-1.75
 function buildNav() {
   return `
   <nav id="site-nav">
-    <a href="index.html" class="nav-logo">
-      <img src="assets/logo.png" alt="House of Calvary">
+    <a href="index.html" class="nav-logo" id="nav-logo-link">
+      <img src="assets/logo.png" alt="" id="nav-logo-img" onerror="this.style.display='none'; document.getElementById('nav-logo-text').style.display='block';">
+      <span class="nav-logo-text" id="nav-logo-text" style="display:none;">House of Calvary †</span>
     </a>
     <ul class="nav-links">
       <li><a href="index.html">Home</a></li>
